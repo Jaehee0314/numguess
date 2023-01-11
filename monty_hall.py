fc3409b81894f1cc9ceac18e22069ff2d2b1ebb5
Jaehee0314 <ran_ed@naver.com>
2023-01-11 10:45:20 +0900

    feat: Add Iteration
    for 10000 times for test

--- monty_hall.py
+++ monty_hall.py
@@ -6,14 +6,15 @@ result = {
     }
 
 doors = [0,0,1] # 0:goat, 1:sports car]
-shuffle(doors)
-#print(doors)
-user_choice = choice(doors)
-#print(user_choice)
-if user_choice ==0:
-    result['move_to_win'] += 1
-else:
-    result['stay_to_win'] += 1
+for _ in range(10000):
+    shuffle(doors)
+    #print(doors)
+    user_choice = choice(doors)
+    #print(user_choice)
+    if user_choice ==0:
+        result['move_to_win'] += 1
+    else:
+        result['stay_to_win'] += 1
 
 print(result)
 
